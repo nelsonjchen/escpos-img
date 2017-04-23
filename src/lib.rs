4c1cb8a60beddfdc2e89d2cdd7754d0f20dd586c
Nelson Chen <crazysim@gmail.com>
2017-04-22 17:07:46 -0700

    Some structs for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 #[macro_use()]
 extern crate nom;
 
+pub enum RasterBitImageMode {
+    Normal,
+    DoubleWidth,
+    DoubleHeight,
+    Quadruple,
+}
+
+pub struct PrintRasterBitImage<'a> {
+    pub mode: RasterBitImageMode,
+    pub x: u16,
+    pub y: u16,
+    pub data: &'a u8,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
